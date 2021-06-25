ebd91e6d3118626ca066c2141e66c2fe01256038
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-06-25 15:22:22 +0200

    Opening X window

--- source/lib_graphics/linux/window_impl.cpp
+++ source/lib_graphics/linux/window_impl.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <stdexcept>
+#include <X11/Xlib.h>
 
 #include "../window_impl.h"
 #include "../opengl.h"
@@ -11,6 +12,7 @@ namespace LibGraphics
 class WindowImpl::Impl
 {
 public:
+    Display *display;
     std::shared_ptr<OpenGl> openGl{nullptr};
     bool isOpen;
 };
@@ -31,6 +33,25 @@ void WindowImpl::create(String const &title)
     {
         throw std::logic_error("Window has already been created");
     }
+
+    // NULL => Use DISPLAY environment variable
+    m_pImpl->display = XOpenDisplay(NULL);
+    if (m_pImpl->display == NULL)
+    {
+	throw std::runtime_error("Failed to connect to X server.");
+    }
+
+    const int screen{DefaultScreen(m_pImpl->display)};
+    const ::Window window = XCreateSimpleWindow(
+        m_pImpl->display,
+	RootWindow(m_pImpl->display, screen),
+	10, 10,
+	100, 100,
+	1,
+	BlackPixel(m_pImpl->display, screen), WhitePixel(m_pImpl->display, screen));
+
+    XMapWindow(m_pImpl->display, window);
+    XFlush(m_pImpl->display);
 
     m_pImpl->openGl = std::make_shared<OpenGlLinux>();
     m_pImpl->isOpen = true;
