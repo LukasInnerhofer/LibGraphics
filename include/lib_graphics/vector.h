45d0388793a4c6f310b0add0bf39beb62ada074d
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-08-05 12:28:30 +0200

    Turned Vector into class and added some operators

--- include/lib_graphics/vector.h
+++ include/lib_graphics/vector.h
@@ -3,9 +3,47 @@
 namespace LibGraphics
 {
 
-typedef struct VectorTag
+template <typename T>
+class Vector
 {
-    unsigned int x, y;
-} Vector;
+public:
+    constexpr Vector() : Vector{0, 0}
+    {
+
+    }
+    constexpr Vector(T x, T y) : m_x{x}, m_y{y}
+    {
+
+    }
+
+    T getX() const
+    {
+        return m_x;
+    }
+    T getY() const
+    {
+        return m_y;
+    }
+
+    Vector operator+(Vector const &rhs) const
+    {
+        return {m_x + rhs.m_x, m_y + rhs.m_y};
+    }
+
+    Vector& operator+=(Vector const &rhs)
+    {
+        m_x += rhs.m_x;
+        m_y += rhs.m_y;
+        return *this;
+    }
+
+    Vector operator-(Vector const &rhs) const
+    {
+        return {m_x - rhs.m_x, m_y - rhs.m_y};
+    }
+
+private:
+    T m_x, m_y;
+};
 
 }
